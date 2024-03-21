39ee75c11c864ff0fc0f0b2fef5a9f96088e37c5
Gordon Zam <gordonzam@gmail.com>
2024-03-22 11:13:02 +1300

    color select for product

--- app/(routes)/product/[productId]/productDetasils.tsx
+++ app/(routes)/product/[productId]/productDetasils.tsx
@@ -1,20 +1,62 @@
 'use client';
 
+import SetColor from '@/app/components/products/setColor';
+import { product } from '@/utils/product';
 import { Rating } from '@mui/material';
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
 interface ProductDetailsProps {
   product: any;
 }
+// type for cartProduct useState
+export type CartProductType = {
+  id: string;
+  name: string;
+  desc: string;
+  category: string;
+  brand: string;
+  selectedImg: SelectedImgType;
+  quantity: number;
+  price: number;
+};
+// creating type for selectedImg in CartProductType
+export type SelectedImgType = {
+  color: string;
+  colorCode: string;
+  image: string;
+};
 
 const Horizontal = () => {
   return <hr className='w-[40%] my-2' />;
 };
 
 const ProductDetails = ({ product }: ProductDetailsProps) => {
+  const [CartProduct, setCartProduct] = useState<CartProductType>({
+    id: product.id,
+    name: product.name,
+    desc: product.description,
+    category: product.category,
+    brand: product.brand,
+    selectedImg: { ...product.images[0] },
+    quantity: 1,
+    price: product.price,
+  });
+
   const productRating =
     product.reviews.reduce((acc: number, item: any) => item.rating + acc, 0) /
     product.reviews.length;
+
+  const handleColorSelect = useCallback(
+    (value: SelectedImgType) => {
+      setCartProduct((prev) => {
+        return { ...prev, selectedImg: value };
+      });
+    },
+    [CartProduct.selectedImg]
+  );
+
+  console.log(CartProduct);
+
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 gap-12'>
       <div>Images</div>
@@ -39,9 +81,16 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
           {product.inStock ? 'In Stock' : 'Out of Stock'}
         </div>
         <Horizontal />
+        <SetColor
+          cartProduct={CartProduct}
+          images={product.images}
+          handleColorSelect={handleColorSelect}
+        />
+        <Horizontal />
 
+        <div>quantity</div>
         <Horizontal />
-        <Horizontal />
+        <div>add to cart</div>
       </div>
     </div>
   );
