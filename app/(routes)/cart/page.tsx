e49d88f924e7d82627976503a08a36aa24bcea28
Gordon Zam <gordonzam@gmail.com>
2024-08-10 17:43:06 +1200

    setting up shopping cart page

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -1,9 +1,17 @@
+import Container from '@/app/components/container';
 import React from 'react';
+import CartClient from './cartClient';
 
 type Props = {};
 
 const Cart = (props: Props) => {
-  return <div>Cart page</div>;
+  return (
+    <div className='pt-8'>
+      <Container>
+        <CartClient />
+      </Container>
+    </div>
+  );
 };
 
 export default Cart;
